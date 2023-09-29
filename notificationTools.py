58de996956d887c4de4276a25f67bcf22f56c323
Samuel Colpaert <86991798+sscolpaert@users.noreply.github.com>
2023-09-29 12:29:53 -0300

    Atualizar o notificationTools.py

--- notificationTools.py
+++ notificationTools.py
@@ -1,10 +1,10 @@
 from winotify import Notification, audio
 
 notificacao = Notification(app_id="Nota", title="Notificação da automação",
-                           msg="Entra no site ai", duration="short")
+                           msg="Olha esse site que daora", duration="short")
 
 notificacao.set_audio(audio.LoopingAlarm, loop=False)
 
-notificacao.add_actions(label="Aprenda Python", launch="https://github.com/sscolpaert/notificationTool")
+notificacao.add_actions(label="Aperta no botão ai", launch="https://github.com/sscolpaert/notificationTool")
 
 notificacao.show()
